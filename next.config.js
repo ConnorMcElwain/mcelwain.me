f92d5e6148725ed589805c791bc41648a7484195
Connor McElwain <connorm1998@gmail.com>
2025-03-03 19:59:08 -0600

    Form Fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.tsx",
 });
 
+require("dotenv").config();
+
 module.exports = withNextra({
   env: {
     FORMBEE_API_KEY: process.env.FORMBEE_API_KEY,
