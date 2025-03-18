f61a4d69365d1e66443114429990e1575f7565c5
Connor McElwain <connorm1998@gmail.com>
2025-03-17 21:09:48 -0500

    Dark mode revert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
     content: [
       './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
       './components/**/*.{js,jsx,ts,tsx,md,mdx}',
